146910a6d8e32e9bf6b611900cd931daa2542ff4
hipzi <zahramillah582@gmail.com>
2021-03-17 14:21:05 +0700

    simple calculator

---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        Double val = 0;
+        String text = "";
+        bool flag_op_press = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            if((output.Text == "0")||(flag_op_press)) 
+            {
+                output.Clear();
+            }
+
+            Button button = (Button)sender;
+            output.Text = output.Text + button.Text;
+            flag_op_press = false;
+        }
+
+        private void ce_button(object sender, EventArgs e)
+        {
+            output.Text = "0";
+        }
+
+        private void op_press(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            text = button.Text;
+            val = Double.Parse(output.Text);
+            flag_op_press = true;
+        }
+
+        private void op_calc(object sender, EventArgs e)
+        {
+            switch (text)
+            {
+                case "+":
+                    output.Text = (val + Double.Parse(output.Text)).ToString();
+                    break;
+                case "-":
+                    output.Text = (val - Double.Parse(output.Text)).ToString();
+                    break;
+                case "/":
+                    output.Text = (val / Double.Parse(output.Text)).ToString();
+                    break;
+                case "*":
+                    output.Text = (val * Double.Parse(output.Text)).ToString();
+                    break;
+                default:
+                    break;
+            }
+            flag_op_press = false;
+        }
+
+        private void c_button(object sender, EventArgs e)
+        {
+            output.Clear();
+            val = 0;
+            output.Text = "0";
+        }
+    }
+}
